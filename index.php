5aaa83616790414e0975cff6ba90e1f40c82a312
Sam Jin <samjin@SamdeMacBook-Air.local>
2017-01-26 18:04:10 +0800

    this is a test update from sam

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WUYE</title>
+</head>
+<body>
+<h1>WuYe Test Site.</h1>
+<?php
+
+echo "<p>This is a first test image created by SamJin.</p><br>";
+echo "<p>Current Date is: ".date("H:i jS F")."!</p>";
+
+?>
+</body>
+</html>
