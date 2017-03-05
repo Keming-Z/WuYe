8af0c81c29ae09df83c3a0665f25f37fc8385b78
Sam Jin <jinhanran1998@126.com>
2017-03-05 17:40:19 +0800

    change

---
+++ Sites/index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>WuYe</title>
+<style type="text/css">
+	
+	/* test css*/
+	.hr-menu{
+		position: relative;
+	}
+	.hr-menu li{
+		position: relative;
+		left: 10px;
+		padding: 3px;
+	}
+	.mainpage .page-intro .title{
+		color: aqua;
+	}
+</style>
+
+</head>
+
+<body>
+<div class="head-menu"><ul>
+	<li><a>HOME PAGE</a></li>
+	<li><a>ABOUT WUYE</a></li>
+	<li><a>PROCEDURES</a></li>
+	<li><a>OVERVIEW</a></li>
+	<li><a>PARTNERSHIP</a></li>
+	<li><a>CONTACT US</a></li>
+	<button class="lang"></button>
+</ul></div>
+
+<div class="background"></div>
+
+<div class="mainpage">
+	<div class="page-title">
+		<div class="title">五叶</div>
+		<div class="subtitle">Enrichment WuYe</div>
+		<div class="info">Enrich your opportunities outside classroom.</div>
+	</div>
+
+	<div class="page-intro">
+		<div class="title">Why Wuye</div>
+		<img class="image">
+		<div class="info">information about wuye...</div>
+	</div>
+
+	<div class="page-infobox">
+	<!-- info image -->
+		<img class="image">
+		<div class="title">职业体验</div>
+		<div class="info">Blablabla</div>
+	</div>
+	
+	<div class="page-infobox">
+	<!-- info image -->
+		<img class="image">
+		<div class="title">职业体验</div>
+		<div class="info">Blablabla</div>
+	</div>
+	
+	<!-- etc. -->
+	
+</div>
+
+<div class="bottom-menu">
+<img class="logo">
+	<ul><li>About WuYe</li>
+		<li>blabla</li>
+		<li>blabla</li>
+	</ul>
+	<ul><li>List 2</li>
+		<li>blabla</li>
+		<li>blabla</li>
+	</ul>
+	<!-- etc. -->
+</div>
+
+</body>
+</html>
